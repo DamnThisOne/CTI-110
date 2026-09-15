e1d91d03421419cbe09a6ccca2a8e94cd4a39bcb
JayB <anonhuman4sumreason@gmail.com>
2026-09-15 13:41:15 -0400

    Add files via upload

---
+++ P1LAB1_JGreene.py
@@ -0,0 +1,9 @@
+# J.Greene
+#9/15/26
+# I/O
+# Get first name from user
+Name1_ = input("Enter your first name: ")
+# get the last one
+Name2_ = input("Enter your last name: ")
+
+print("Hello,", Name1_, Name2_ + "!", "Welcome to CTI-110")
