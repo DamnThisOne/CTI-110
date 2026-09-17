b331f3d008d238c504de43cf9a1a582230877d1d
JayB <anonhuman4sumreason@gmail.com>
2026-09-17 19:06:51 -0400

    Add files via upload

---
+++ P1HW1_JGreene.py
@@ -0,0 +1,31 @@
+# J.Greene
+# 9/15/26
+
+
+print("---Calculate Exponents---")
+base= input("Enter base number: ")
+exponent= input("Enter the exponent: ")
+base= int(base)
+exponent= int(exponent)
+hate= base ** exponent
+
+print("")
+print(base, "raised to the power of", exponent,"equals",hate,"!")
+print("")
+print("---Addition and Subtraction---")
+
+print("")
+
+start= input("Enter starting number: ")
+add= input("Enter a number to add: ")
+subtract= input("Enter a number to subtract: ") 
+
+start= int(start)
+add= int(add)
+subtract= int(subtract)
+
+A= start + add - subtract
+print("")
+print(start,"+",add,"-",subtract,"is equal to",A)
+print("")
+
